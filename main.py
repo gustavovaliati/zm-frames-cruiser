ce6ad41cba8dcd6c4500623047c2e15c3e2f31ab
Gustavo Valiati <gustavovaliati@gmail.com>
2018-01-06 16:38:06 -0200

    first motion experiment

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QAction, QFileDialog, QApplication, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QShortcut, QSpinBox, QFrame)
-from PyQt5.QtGui import QPixmap, QKeySequence
+from PyQt5.QtGui import QPixmap, QKeySequence, QPainter, QPen
 from PyQt5 import QtCore
 
 import sys, glob, os, argparse
+
+MOTION_SUPPORT = False
+try:
+    import cv2, imutils
+    MOTION_SUPPORT = True
+except ImportError:
+    print("Motion packages are not installed.")
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--directory",
+                required = False,
+                default = False,
+                help = "The directory containing the images.",
+                dest='workdir')
+
+ARGS = vars(ap.parse_args())
 
 class ControlsWidget(QWidget):
     def __init__(self, parent):
@@ -72,11 +88,64 @@ class ImageWidget(QWidget):
 
         self.setLayout(hbox)
 
+        self.previousFrame = None
+
+    def loadFrame(self, index):
+        frame = cv2.imread(self.imageList[index])
+        frame = imutils.resize(frame, width=500)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        return gray
+
     def loadImage(self, index):
-        self.pixmap.load(self.imageList[index])
-        self.lbl.setPixmap(self.pixmap)
-        statusBarMessage = '[{}/{}] {}'.format(index+1, self.imageListLength, self.imageList[index])
+        impath = self.imageList[index]
+        self.pixmap.load(impath)
+
+        statusBarMessage = '[{}/{}] {}'.format(index+1, self.imageListLength+1, impath)
         self.parent.statusBar().showMessage(statusBarMessage)
+
+        if MOTION_SUPPORT:
+
+            gray = self.loadFrame(index)
+
+            if index != 0 :
+
+                # compute the absolute difference between the current frame and
+                # first frame
+                frameDelta = cv2.absdiff(self.previousFrame, gray)
+                thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+                # dilate the thresholded image to fill in holes, then find contours
+                # on thresholded image
+                thresh = cv2.dilate(thresh, None, iterations=2)
+                (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                # loop over the contours
+                for c in cnts:
+                    # if the contour is too small, ignore it
+                    if cv2.contourArea(c) < 500:
+                        continue
+
+                    # compute the bounding box for the contour, draw it on the frame,
+                    # and update the text
+                    (resized_x, resized_y, resized_w, resized_h) = cv2.boundingRect(c)
+
+                    x = int(resized_x / 0.78125) # proportion: 500*100 / 640
+                    y = int(resized_y / 0.78125)
+                    w = int(resized_w / 0.78125)
+                    h = int(resized_h / 0.78125)
+
+                    self.painter = QPainter()
+                    self.painter.begin(self.pixmap)
+                    self.painter.setPen(QtCore.Qt.red)
+                    self.painter.drawRect(x,y,w,h)
+                    self.painter.end()
+
+            self.previousFrame = gray
+
+        self.lbl.setPixmap(self.pixmap)
 
     def goToImage(self, localIndex):
         if localIndex >= 0 and localIndex < self.imageListLength:
@@ -154,5 +223,5 @@ if __name__ == '__main__':
     args = vars(ap.parse_args())
 
     app = QApplication(sys.argv)
-    ex = MainWindow(args['workdir'])
+    ex = MainWindow(ARGS['workdir'])
     sys.exit(app.exec_())
